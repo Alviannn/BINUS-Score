3541deac3f591519d4e32d2981e6de107ae7f410
Alviannn <alvianndq@gmail.com>
2021-08-16 13:33:34 +0700

    Changed score output to be printed in a form of a table

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ import json
 import binmay
 
 if __name__ == '__main__':
+    print('Logging in to BINUSMaya...')
     binmay.login()
 
     period = binmay.choose_period()
-    score = binmay.view_score(period)
+    score_list = binmay.view_score(period)
 
-    print(json.dumps(score, indent=4))
+    table_line = '+----+------------------------------------------+-------+-------+'
+    header_format = '| %-2s | %-40s | %-5s | %-5s |'
+    row_format = '| %2d | %-40s | %5s | %-5s |'
+
+    print(table_line)
+    print(header_format % ('No', 'Course Name', 'Score', 'Grade'))
+    print(table_line)
+
+    count = 0
+    for tmp in score_list:
+        count += 1
+        print(row_format % (count, tmp['course'], str(tmp['final-score']), tmp['grade']))
+
+    print(table_line)
