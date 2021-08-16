52c6313c026c6fa533d97a6e7ff09ff4fa461148
Alviannn <alvianndq@gmail.com>
2021-08-16 16:00:08 +0700

    Added error message on any failure

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import binmay
 
+from const import LoginError
 from colorama import Fore
 from time import sleep
+
 
 def main():
     os.system('cls')
@@ -12,8 +14,21 @@ def main():
         binmay.login()
         print(f'{Fore.LIGHTGREEN_EX}Successfully logged in to BINUSMaya!')
         sleep(2)
-    except Exception:
-        print(f'{Fore.LIGHTRED_EX}Failed to login to BINUSMaya!')
+    except LoginError as ex:
+        if ex.code == LoginError.SCRAPE_FAIL:
+            print(
+                f'{Fore.LIGHTRED_EX}Failed to scrape BINUSMaya!\n'
+                'Possible cause:\n'
+                f'{Fore.LIGHTYELLOW_EX}* {Fore.LIGHTRED_EX}The website could be under maintenance, visit the site to make sure!\n'
+                f'{Fore.LIGHTYELLOW_EX}* {Fore.LIGHTRED_EX}Your internet might be down, check your internet connection!')
+        elif ex.code == LoginError.INCORRECT_CREDENTIALS:
+            print(
+                f'{Fore.LIGHTRED_EX}Incorrect username or password! Please check it again!')
+        elif ex.code == LoginError.UNKNOWN:
+            print(
+                f'{Fore.LIGHTRED_EX}An unknown error has occurred that causes the program to fail to login!'
+                'Please contact the developer!')
+
         os.system('pause')
         return
 
@@ -33,13 +48,15 @@ def main():
         count = 0
         for tmp in score[0]:
             count += 1
-            print(row_format % (count, tmp['course'], str(tmp['final-score']), tmp['grade']))
+            print(row_format % (count, tmp['course'], str(
+                tmp['final-score']), tmp['grade']))
 
         print('+-----+------------------------------------------+-------+-------+')
         print('| %-3s | %-56s |' % ('GPA', str(score[1])))
         print('+-----+----------------------------------------------------------+')
 
         os.system('pause')
+
 
 if __name__ == '__main__':
     main()
