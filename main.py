d181e6aa9ad94b501d9738c8df73dca6dde3948f
Alviannn <alvianndq@gmail.com>
2021-08-16 14:10:03 +0700

    Improved output
    * Added GPA result

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-import json
+import os
 import binmay
+from colorama import Fore
 
 if __name__ == '__main__':
-    print('Logging in to BINUSMaya...')
+    os.system('cls')
+
+    print(f'{Fore.GREEN}Logging in to BINUSMaya...{Fore.RESET}')
     binmay.login()
 
     period = binmay.choose_period()
-    score_list = binmay.view_score(period)
+    score = binmay.view_score(period)
 
-    table_line = '+----+------------------------------------------+-------+-------+'
-    header_format = '| %-2s | %-40s | %-5s | %-5s |'
-    row_format = '| %2d | %-40s | %5s | %-5s |'
+    header_format = '| %-3s | %-40s | %-5s | %-5s |'
+    row_format = '| %3d | %-40s | %5s | %-5s |'
 
-    print(table_line)
+    print('+-----+------------------------------------------+-------+-------+')
     print(header_format % ('No', 'Course Name', 'Score', 'Grade'))
-    print(table_line)
+    print('+-----+------------------------------------------+-------+-------+')
 
     count = 0
-    for tmp in score_list:
+    for tmp in score[0]:
         count += 1
         print(row_format % (count, tmp['course'], str(tmp['final-score']), tmp['grade']))
 
-    print(table_line)
+    print('+-----+------------------------------------------+-------+-------+')
+    print('| %-3s | %-56s |' % ('GPA', str(score[1])))
+    print('+-----+----------------------------------------------------------+')
